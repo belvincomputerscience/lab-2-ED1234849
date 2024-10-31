252d13558032e05344be469fd53831c482496e0f
ED1234849 <realbrosix@gmail.com>
2024-10-31 14:26:35 +0000

    lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,1 +1,26 @@
+// Name: Ethan De La Rosa
 
+#include <iostream>
+using namespace std;
+//User enters the grades to get the average
+int main() {
+    // Declairing  the variables for the grades
+    double grade1, grade2, grade3, grade4;
+    //telling the user to enter the grades
+    cout << "Please enter the first grade: ";
+    cin >> grade1;
+    cout << "PLease enter the second grade: ";
+    cin >> grade2;
+    cout << "PLease enter the Third  grade: ";
+    cin >> grade3;
+    cout << "PLease enter the fourth grade: ";
+    cin >> grade4;
+
+    //Now to calculate the average grade
+    double average = (grade1 + grade2 + grade3 + grade4) / 4.0;
+
+    //Now to display the grade with a decimal
+    cout << "The average garde is " << average << endl;
+
+    return 0;
+}
